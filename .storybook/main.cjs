f14cf650e8730287bda799904e6746820dfa0f50
노이(현은비) <silverain66@belivvr.com>
2022-12-14 14:27:45 +0900

    fix config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -22,7 +22,7 @@ module.exports = {
   },
   async viteFinal(config, { configType }) {
     return mergeConfig(config, {
-      base: './',
+      base: '/pages-actions/',
       resolve: {
         alias: [
           { find: '@', replacement: resolve(__dirname, '../src') },
